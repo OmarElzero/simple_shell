17397b45798e0f9ec033370a91226cd16b196521
root <root@DESKTOP-IVHRL9V.localdomain>
2023-11-16 05:00:01 +0200

    yaaaaaaa

--- exec.c
+++ exec.c
@@ -1,5 +1,4 @@
 #include "MFheader.h"
-
 /**
  * execution - func that execute prog
  * @pathname: full path of executable command
@@ -8,54 +7,46 @@
  * @environ: environment variable
  * no return void func
  */
-
 void execution(char *pathname, char **argv, char *linebuff, char **environ)
 {
-    pid_t chpid;
+	pid_t chpid;
+	/**/
+	if (pathname == NULL)
+	{
+		perror("command");
+		return;
+	}
+	int status;
 
-    if (pathname == NULL)
-    {
-        perror("command");
-        return;
-    }
-
-    chpid = fork();
-
-    if (chpid == -1)
-    {
-        perror("fork");
-        freenfr(linebuff);
-        freenfr(pathname);
-        exit(EXIT_FAILURE);
-    }
-
-    if (chpid == 0)
-    {
-        
-        if (strcmp(pathname, "/bin/ls") == 0)
-        {
-            printf("Executing /bin/ls...\n");
-            
-        }
-
-        if (execve(pathname, argv, environ) == -1)
-        {
-            perror("execve");
-            fprintf(stderr, "Error executing command: %s\n", argv[0]);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        int status;
-
-        wait(&status);
-
-        if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
-        {
-            fprintf(stderr, " exit status %d\n", WEXITSTATUS(status));
-        }
-    }
-
-    freenfr(pathname);
+	chpid = fork();
+	if (chpid == -1)
+	{
+		perror("fork");
+		freenfr(linebuff);
+		freenfr(pathname);
+		exit(EXIT_FAILURE);
+	}
+	if (chpid == 0)
+	{
+		if (strcmp(pathname, "/bin/ls") == 0)
+		{
+			printf("Executing /bin/ls...\n");
+		}
+		if (execve(pathname, argv, environ) == -1)
+		{
+			perror("execve");
+			fprintf(stderr, "Error executing command: %s\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}
+		fprintf(stderr, "Command not found: %s\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+wait(&status);
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+		{
+			fprintf(stderr, " exit status %d\n", WEXITSTATUS(status));
+		}
+	}	freenfr(pathname);
 }
